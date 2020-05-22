b1d0fa714536df805774233931da6cb10d01d7ca
dengaye <1277050972@qq.com>
2020-05-22 10:34:39 +0800

    feat: add babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
                 test: /\.css$/,
                 use: [
                     'style.loader',
